49f5b61e3567938e0ba2a6290a7859cfc74de060
Yosif Georgiev <iodesign@live.com>
2021-06-09 20:25:41 +0300

    photos fix

--- pages/gallery.js
+++ pages/gallery.js
@@ -3,7 +3,30 @@ import PhotoGallery from '../components/PhotoGallery'
 const PictureGallery = () => {
     return ( 
         <main>
-            <PhotoGallery />
+            <h1>Галерия със снимки</h1>
+            <div className="gallery">
+                <PhotoGallery />
+            </div>
+            <style jsx>{`
+                main{
+                    max-width:1200px;
+                    margin:0 auto;
+                }
+                h1{
+                    color:#e9e9e7;
+                }
+                .gallery{
+                    margin: 0px 0px 40px 0px;
+                }
+                @media only screen and (max-width: 1239px) {
+                    h1{
+                        margin: 20px 20px;
+                    }
+                    .gallery{
+                        margin: 20px 20px;
+                    }
+                }
+            `}</style>
         </main>
      );
 }
