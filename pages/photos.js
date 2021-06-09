76dc27be83e5bee89eb036d89782af1e9254e0f3
Yosif Georgiev <iodesign@live.com>
2021-06-09 20:08:47 +0300

    photos fix

--- pages/photos.js
+++ pages/photos.js
@@ -1,4 +1,4 @@
-export const photos = [
+const photos = [
     {
     //   src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
       src: "/pics/IMG_5784.jpeg",
@@ -53,4 +53,6 @@ export const photos = [
                                 }, 
  
   ];
+
+export default photos
   
