8325975a3602439b6e60eb24a32ad2b978cf4e65
Yosif Georgiev <iodesign@live.com>
2021-06-24 16:51:21 +0300

    add SEO image

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,7 @@ class MyDocument extends Document {
                 <link rel="preload"/>
                 <meta name="theme-color" content="#33333"/>
                 <meta property="og:image" content="https://www.barberscrew.bg/Logo_Barbers_Crew.webp"/> 
-                <meta property="og:image:type" content="image/png"/>
+                <meta property="og:image:type" content="image/webp"/>
                 <meta property="og:image:width" content="200"/>
                 <meta property="og:image:height" content="200"/>
                 <meta name="description" content="Мъжко Подстригване, Бръснене, Мъжки Прически, Стайлинг на Коса и Грижа за Брадата - Барбершоп Варна - Barbers Crew"/>
