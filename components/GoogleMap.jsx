4b68a767caf99aeac8146c45fb6de965a66ad36c
Joszeph <45237732+Joszeph@users.noreply.github.com>
2021-12-14 17:41:25 +0200

    Add files via upload
    Update Google Map Address

--- components/GoogleMap.jsx
+++ components/GoogleMap.jsx
@@ -2,7 +2,7 @@ const GoogleMap =()=> {
 
       return(
         <div className="map-responsive">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2907.694519911896!2d28.011773786988055!3d43.231750003830186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40a4576ca86d97c3%3A0x377378550b1b47eb!2sBarbers%20CREW%20st.Constantine%20and%20Helena!5e0!3m2!1sen!2sbg!4v1623155323612!5m2!1sen!2sbg"
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d727.0504494866881!2d27.918088729229453!3d43.20525657623532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40a45315b0d2b723%3A0xf997be351435c738!2sBarbers%20Crew%20Varna!5e0!3m2!1sen!2sbg!4v1639489501271!5m2!1sen!2sbg"
          width="100%" height="450" style={{border:0}} allowfullscreen="" loading="lazy"></iframe>
         <style jsx>{`
          
@@ -13,3 +13,4 @@ const GoogleMap =()=> {
 
 
 export default GoogleMap
+
