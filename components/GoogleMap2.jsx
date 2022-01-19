44a068ab8d883052074a2df8c270c61bb961c2db
Joszeph <45237732+Joszeph@users.noreply.github.com>
2022-01-19 19:11:14 +0200

    Add files via upload

---
+++ components/GoogleMap2.jsx
@@ -0,0 +1,17 @@
+const GoogleMap2 =()=> {
+
+    return(
+      <div className="map-responsive">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d769.9355051147304!2d28.01169965641501!3d43.232775470455245!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40a4576ca86d97c3%3A0x377378550b1b47eb!2sBarbers%20CREW%20st.Constantine%20and%20Helena!5e0!3m2!1sen!2sbg!4v1642609693311!5m2!1sen!2sbg"
+       width="100%" height="450" style={{border:0}} allowfullscreen="" loading="lazy"></iframe>
+      <style jsx>{`
+       
+      `}</style>
+      </div>
+    )
+  }
+
+
+export default GoogleMap2
+
+
