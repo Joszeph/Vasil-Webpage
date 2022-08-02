bc964938c3d2f1a621fbfa470172bc3a7077eb4d
Joseph <45237732+Joszeph@users.noreply.github.com>
2022-08-02 14:16:23 +0300

    Update RightNavBar.jsx

--- components/Nav/RightNavBar.jsx
+++ components/Nav/RightNavBar.jsx
@@ -69,7 +69,7 @@ const RightNavBar = (props) => {
       <li className={router.pathname == "/prices" ? "active" : ""}><Link href="/prices"><a onClick={() => setOpen(!open)}>{t.activities}</a></Link></li>
       <li className={router.pathname == "/clients" ? "active" : ""}><Link href="/gallery"><a onClick={() => setOpen(!open)}>{t.gallery}</a></Link></li>
       <li className={router.pathname == "/contacts" ? "active" : ""}><Link href="/contacts"><a onClick={() => setOpen(!open)}>{t.contacts}</a></Link></li>
-      <li className={router.pathname == "/" ? "active" : ""}><Link href="https://calendly.com/barbers_crew/booking"><a target="_blank" onClick={() => setOpen(!open)}><div className="reservationBtn">{t.appointment}</div></a></Link></li>
+      <li className={router.pathname == "/" ? "active" : ""}><Link href="https://calendly.com/barbers_crew/app"><a target="_blank" onClick={() => setOpen(!open)}><div className="reservationBtn">{t.appointment}</div></a></Link></li>
     </Ul>
   )
 }
